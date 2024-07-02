0a4173985f59914d82b899173854857d87fb092c
ssunchu10 <sumitsunchu@gmail.com>
2024-07-02 13:34:11 -0500

    Backend Integrated

--- src/apis/weatherApi.js
+++ src/apis/weatherApi.js
@@ -2,12 +2,12 @@ import axios from "axios"
 
 export const getWeather = async (location) => {
     try {
-        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=630b47da595bd59784417268bdd1d863&units=imperial`);
+        // const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=630b47da595bd59784417268bdd1d863&units=imperial`);
+        const response = await axios.get(`http://localhost:8000/weather?query=${location}`);
         console.log(response.data);
         return response.data;
     } catch (error) {
         console.log("There is an error! ", error.message);
         return null;
     }
-
-}
+}   
--- src/components/WeatherData/WeatherData.js
+++ src/components/WeatherData/WeatherData.js
@@ -7,64 +7,72 @@ const WeatherData = () => {
   const data = searchState.searchResults;
 
   return (
-    <div className="data-container">
-      <div className="top-container">
-        <div className="location-container">
-          <p>{data.name}</p>
-          <div className="temp">
-            {data.main ? <h1>{Math.round(data.main.temp)}°F</h1> : null}
+    <div>
+      <div className="data-container">
+        <div className="top-container">
+          <div className="location-container">
+            <p>{data.name}</p>
+            <div className="temp">
+              {data.main ? <h1>{Math.round(data.main.temp)}°F</h1> : null}
+            </div>
+          </div>
+          <div className="description-container">
+            {data.weather ? (
+              <img
+                className="weather-icon-container"
+                src={searchState.iconUrl}
+                alt="weather-icon"
+              />
+            ) : null}
+            {data.weather ? (
+              <p className="">{data.weather[0].description}</p>
+            ) : null}
           </div>
         </div>
-        <div className="description-container">
-          {data.weather ? (
-            <img
-              className="weather-icon-container"
-              src={searchState.iconUrl}
-              alt="weather-icon"
-            />
-          ) : null}
-          {data.weather ? (
-            <p className="">{data.weather[0].description}</p>
-          ) : null}
+        <div className="bottom-container">
+          <div className="bottom-description-container">
+            <div className="feels">
+              {data.main ? (
+                <p className="bold">{Math.round(data.main.feels_like)}°F</p>
+              ) : null}
+              {data.main ? <p>Feels Like</p> : null}
+            </div>
+            <div className="humidity">
+              {data.main ? <p className="bold">{data.main.humidity}%</p> : null}
+              {data.main ? <p>Humidity</p> : null}
+            </div>
+            <div className="wind">
+              {data.wind ? (
+                <p className="bold">{Math.round(data.wind.speed)}MPH</p>
+              ) : null}
+              {data.wind ? <p>Wind Speed</p> : null}
+            </div>
+          </div>
+          <div className="bottom-description-container">
+            <div className="sunrise">
+              {data.sys ? (
+                <p className="bold">
+                  {convertUnixTime(data.sys.sunrise, data.timezone)}
+                </p>
+              ) : null}
+              {data.sys ? <p>Sunrise</p> : null}
+            </div>
+            <div className="sunset">
+              {data.sys ? (
+                <p className="bold">
+                  {convertUnixTime(data.sys.sunset, data.timezone)}
+                </p>
+              ) : null}
+              {data.sys ? <p>Sunset</p> : null}
+            </div>
+          </div>
         </div>
       </div>
-      <div className="bottom-container">
-        <div className="bottom-description-container">
-          <div className="feels">
-            {data.main ? (
-              <p className="bold">{Math.round(data.main.feels_like)}°F</p>
-            ) : null}
-            {data.main ? <p>Feels Like</p> : null}
-          </div>
-          <div className="humidity">
-            {data.main ? <p className="bold">{data.main.humidity}%</p> : null}
-            {data.main ? <p>Humidity</p> : null}
-          </div>
-          <div className="wind">
-            {data.wind ? (
-              <p className="bold">{Math.round(data.wind.speed)}MPH</p>
-            ) : null}
-            {data.wind ? <p>Wind Speed</p> : null}
-          </div>
-        </div>
-        <div className="bottom-description-container">
-          <div className="sunrise">
-            {data.sys ? (
-              <p className="bold">
-                {convertUnixTime(data.sys.sunrise, data.timezone)}
-              </p>
-            ) : null}
-            {data.sys ? <p>Sunrise</p> : null}
-          </div>
-          <div className="sunset">
-            {data.sys ? (
-              <p className="bold">
-                {convertUnixTime(data.sys.sunset, data.timezone)}
-              </p>
-            ) : null}
-            {data.sys ? <p>Sunset</p> : null}
-          </div>
-        </div>
+      <div className="button">
+            {data.name ? 
+      <button className="add-button">
+              ADD
+      </button> : null}
       </div>
     </div>
   );
